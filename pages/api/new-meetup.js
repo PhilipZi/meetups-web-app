479aabb87e59a26f8cac491e3631115678b9823f
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-24 22:35:07 +0100

    add dotenv package

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,14 @@
+import { MongoClient } from "mongodb";
+
 function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    MongoClient.connect(
+      "mongodb+srv://philip:<password>@cluster0.cihy4.mongodb.net/?retryWrites=true&w=majority"
+    );
   }
 }
 export default handler;
