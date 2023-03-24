9cc1ecbd990f04c198637f7fd86db82f18b955af
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-24 21:28:22 +0100

    add getStaticPaths func to dynamic page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetailsPage() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://www.wissenschaft.de/wp-content/uploads/i/S/iStock-973113474-1.jpg",
+        id: meetupId,
+        title: "A first Meetup",
+        address: "Some Street 6, some city ",
+        description: "The Meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetailsPage;
