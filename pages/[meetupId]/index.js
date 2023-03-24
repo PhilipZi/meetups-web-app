f5d2072780ea0082c06369a9324475c4b9f949c9
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-24 16:48:50 +0100

    add meetupdetail component and create dynamic routing

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+function MeetupDetails() {
+  return (
+    <Fragment>
+      <img
+        src="https://www.wissenschaft.de/wp-content/uploads/i/S/iStock-973113474-1.jpg"
+        alt="a city picture"
+      />
+      <h1>A first Meetup</h1>
+      <address>Some Street 6, some city </address>
+      <p>The Meetup description</p>
+    </Fragment>
+  );
+}
+
+export default MeetupDetails;
